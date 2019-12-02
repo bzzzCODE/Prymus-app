0ddbc2b295a20f0a055242b2d29fadd0aa83900d
gurgul <gurgul@d17-l427-pc-12.ipa.ki.agh.edu.pl>
2019-12-02 09:01:16 +0100

    sad happy button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{title ? "Happy" : "Sad"}}</h1>
+    <button @click="iAmClicked()">Click me!</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {title: false}
+    },
+    methods: {
+      iAmClicked() {
+        this.title = !this.title;
+      },
+    }
+  };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
