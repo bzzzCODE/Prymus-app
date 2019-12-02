ca6d25f94092cf311890e3bf9ff461a5db0f0cba
gurgul <gurgul@d17-l427-pc-12.ipa.ki.agh.edu.pl>
2019-12-02 09:16:54 +0100

    vif and vfor

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <h1>{{title ? "Happy" : "Sad"}}</h1>
-    <button @click="iAmClicked()">Click me!</button>
+    <ul v-if="numbers.length > 0 && numbers.length < 10">
+      <li :key="number" v-for="number in numbers">{{ number }}</li>
+    </ul>
+    <button @click="addNew()">Add new random number</button>
   </div>
 </template>
 
 <script>
   export default {
     data() {
-      return {title: false}
+      return {numbers: []}
     },
     methods: {
-      iAmClicked() {
-        this.title = !this.title;
-      },
+      addNew() {
+        this.numbers.push(Math.round(Math.random() * 100));
+      }
     }
   };
 </script>
+
