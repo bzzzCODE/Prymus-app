143b9a2026a6ce1980b25ad47998fc840e5de44e
gurgul <gurgul@d17-l427-pc-12.ipa.ki.agh.edu.pl>
2019-12-02 09:29:40 +0100

    participants list

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-    <ul v-if="numbers.length > 0 && numbers.length < 10">
-      <li :key="number" v-for="number in numbers">{{ number }}</li>
-    </ul>
-    <button @click="addNew()">Add new random number</button>
+    <div>
+      <h1>Participants list</h1>
+      <ul v-if="participants.length > 0">
+        <li> :key="participant" v-for="participant in participants">{{ participants }}</li>
+      </ul>
+    </div>
+
+    <h3>New participant</h3>
+    <form @submit.prevent="addNewParticipant()">
+      <label>Firstname</label>
+      <input type="text">
+      <br>
+      <label>Lastname</label>
+      <input type="text">
+      <button>Add new participant</button>
+    </form>
   </div>
 </template>
 
 <script>
   export default {
     data() {
-      return {numbers: []}
+      return {participants: []};
     },
     methods: {
-      addNew() {
-        this.numbers.push(Math.round(Math.random() * 100));
+      addNewParticipant() {
+        this.participants.push(Firstname)
       }
     }
   };
