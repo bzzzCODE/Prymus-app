2822265a4b13f133ef88e7373d04c1637b803238
gurgul <gurgul@d17-l427-pc-12.ipa.ki.agh.edu.pl>
2019-12-09 09:02:16 +0100

    dziala formularz

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <div>
     <div>
       <h1>Participants list</h1>
-      <ul v-if="participants.length > 0">
-        <li> :key="participant" v-for="participant in participants">{{ participants }}</li>
-      </ul>
+      <ol v-if="participants.length > 0">
+        <li :key="person" v-for="person in participants">{{person}}</li>
+      </ol>
     </div>
 
     <h3>New participant</h3>
     <form @submit.prevent="addNewParticipant()">
       <label>Firstname</label>
-      <input type="text">
+      <input type="text" v-model="firstname">
       <br>
       <label>Lastname</label>
-      <input type="text">
+      <input type="text" v-model="lastname">
       <button>Add new participant</button>
     </form>
   </div>
@@ -22,11 +22,13 @@
 <script>
   export default {
     data() {
-      return {participants: []};
+      return {participants: [], firstname: "", lastname: ""};
     },
     methods: {
       addNewParticipant() {
-        this.participants.push(Firstname)
+        if(this.firstname.length > 0 && this.lastname.length > 0) this.participants.push(this.firstname +" "+ this.lastname),
+                this.firstname="",
+                this.lastname="";
       }
     }
   };
